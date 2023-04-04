ef3bc1759b7978311822402ae3c8c02f977da4bd
pablozuta <pablo.zuta.h@gmail.com>
2023-04-03 20:03:08 -0400

    se agregan loc proyecto 'basic go sqlite'

--- basic-go-sqlite/go.mod
+++ basic-go-sqlite/go.mod
@@ -2,4 +2,8 @@ module go-sqlite
 
 go 1.19
 
-require github.com/mattn/go-sqlite3 v1.14.16 // indirect
+require (
+	github.com/360EntSecGroup-Skylar/excelize v1.4.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+)
--- basic-go-sqlite/main.go
+++ basic-go-sqlite/main.go
@@ -3,52 +3,101 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	// Open a connection to the database
+	// abre una connecion con la base de datos
 	db, err := sql.Open("sqlite3", "example.db")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
-	// Create a table if it doesn't already exist
+	// crea una tabla si no existe
 	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, email TEXT)")
 	if err != nil {
 		panic(err)
 	}
 
-	// Insert a new row into the table
-	_, err = db.Exec("INSERT INTO users(name, email) VALUES (?, ?)", "Alice Coltrane", "alice@gmail.com")
-	if err != nil {
-		panic(err)
-	}
+	// crea un loop que pregunta al usuario por input
+	for {
+		fmt.Println("1. AGREGAR USUARIO")
+		fmt.Println("2. BORRAR USUARIO")
+		fmt.Println("3. MOSTRAR USUARIOS")
+		fmt.Println("4. SALIR")
+		fmt.Print("INGRESA TU OPCION: ")
+        
+		// almacena numero en variable
+		var choice int
+		fmt.Scan(&choice)
 
-	// Query the table for all rows
-	rows, err := db.Query("SELECT id, name, email FROM users")
-	if err != nil {
-		panic(err)
-	}
-	defer rows.Close()
+		switch choice {
+		case 1:
+			fmt.Print("Enter name: ")
+			var name string
+			fmt.Scan(&name)
 
-	// Iterate over the rows and print the results
-	for rows.Next() {
-		var id int
-		var name string
-		var email string
-		err = rows.Scan(&id, &name, &email)
-		if err != nil {
-			panic(err)
+			fmt.Print("Enter email: ")
+			var email string
+			fmt.Scan(&email)
+
+			_, err = db.Exec("INSERT INTO users(name, email) VALUES (?, ?)", name, email)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error al agregar usuario: %v\n", err)
+			} else {
+				fmt.Println("Usuario Agregado Correctamente.")
+			}
+		case 2:
+			fmt.Print("Enter user ID: ")
+			var id int
+			fmt.Scan(&id)
+
+			result, err := db.Exec("DELETE FROM users WHERE id = ?", id)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error deleting user: %v\n", err)
+			} else {
+				rowsAffected, _ := result.RowsAffected()
+				if rowsAffected == 0 {
+					fmt.Println("User not found.")
+				} else {
+					fmt.Println("User deleted successfully.")
+				}
+			}
+		case 3:
+			rows, err := db.Query("SELECT id, name, email FROM users")
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error showing users: %v\n", err)
+			} else {
+				defer rows.Close()
+
+				fmt.Println("ID\tNombre\tEmail")
+				for rows.Next() {
+					var id int
+					var name string
+					var email string
+					err = rows.Scan(&id, &name, &email)
+					if err != nil {
+						fmt.Fprintf(os.Stderr, "Error showing users: %v\n", err)
+						break
+					}
+					fmt.Printf("%d\t%s\t%s\n", id, name, email)
+				}
+
+				err = rows.Err()
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "Error showing users: %v\n", err)
+				}
+			}
+		case 4:
+			fmt.Println("HASTA LUEGO!")
+			return
+		default:
+			fmt.Println("Invalid choice. Please try again.")
 		}
-		fmt.Printf("ID: %d, Name: %s, Email: %s\n", id, name, email)
-	}
 
-	// Check for errors during iteration
-	err = rows.Err()
-	if err != nil {
-		panic(err)
+		fmt.Println()
 	}
 }
