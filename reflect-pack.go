be3de6ac43376303d56f20ef8d4391f6fc431ae6
pablozuta <pablo.zuta.h@gmail.com>
2023-04-08 02:49:34 -0400

    se agregan loc a modulo 'reflect package'

--- reflect-pack.go
+++ reflect-pack.go
@@ -8,11 +8,25 @@ import (
 func main() {
 	var numero int = 23
 	fmt.Println(reflect.TypeOf(numero))
+	fmt.Println(reflect.ValueOf(numero))
 	
 	var numeroFloat float32 = 5.655
 	fmt.Println(reflect.TypeOf(numeroFloat))
 	
 	var mentira bool = false
 	fmt.Println(reflect.TypeOf(mentira))
+	
+	var numeroU uint8 = 34
+	fmt.Println(reflect.TypeOf(numeroU))
+
+
+	// arrays 
+	var miArray = [] int {23, 56}
+	fmt.Println(reflect.TypeOf(miArray))
+	
+	var miArrayU = [] uint32 {34, 77, 65, 23}
+	fmt.Println(reflect.TypeOf(miArrayU))
+	fmt.Println(reflect.ValueOf(miArrayU))
+
 
 }
