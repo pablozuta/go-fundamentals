2024aeb9c886091dae2d40cfbf5081ea1c23f57c
pablozuta <pablo.zuta.h@gmail.com>
2023-04-11 22:09:41 -0400

    se agrega carpeta de proyectos 'fiber' y proyecto 'simple server'

---
+++ fiber/simple-server/main.go
@@ -0,0 +1,21 @@
+package main  
+  
+import (  
+fiber "github.com/gofiber/fiber/v2"  
+)  
+  
+func main() {  
+	// crear una instancia de fiber  
+	app := fiber.New()  
+	  
+	// definimos una ruta 
+	app.Get("/", func(c *fiber.Ctx) error {  
+	return c.SendString("Hola desde FIBER")  
+	})  
+	  
+	// iniciamos el servidor en el puerto 3000 
+	err := app.Listen(":3000")  
+	if err != nil {  
+	panic(err)  
+	}  
+}
