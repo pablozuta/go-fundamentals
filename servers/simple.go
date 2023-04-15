7eddfa0715d2050dc864c119b90266b8a23ebeb3
pablozuta <pablo.zuta.h@gmail.com>
2023-04-15 03:31:13 -0400

    se agregan loc

--- servers/simple.go
+++ servers/simple.go
@@ -7,9 +7,14 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hola desde golang server")
+		fmt.Fprintf(w, "<h1>Home Page </h1>")
 	})
 
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<h1>This is the about page</h1>")
+	})
+	
 	fmt.Println("escuchando en puerto :8080")
 	http.ListenAndServe(":8080", nil)
+
 }
