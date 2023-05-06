9ecd554ad6ba542e8144eadf4b0017425fde2501
pablozuta <pablo.zuta.h@gmail.com>
2023-05-05 22:13:53 -0400

    se agregan loc a modulo 'read line' carpeta 'bufio'

--- bufio/read-line.go
+++ bufio/read-line.go
@@ -9,8 +9,14 @@ import (
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
-	fmt.Print("Enter Text:")
+	fmt.Print("Enter Text: ")
 	scanner.Scan()
 	text := scanner.Text()
-	fmt.Printf("You entered: %v\n ", text)
+	fmt.Printf("You entered '%v'\n", text)
+
+	scannerDos := bufio.NewScanner(os.Stdin)
+	fmt.Print("Enter more text: ")
+	scannerDos.Scan()
+	text = scannerDos.Text()
+	fmt.Printf("Now you entered '%v'", text)
 }
