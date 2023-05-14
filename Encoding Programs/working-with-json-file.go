e7b82cfce304698a58dc6cbfa9c82e53021e14cb
pablozuta <pablo.zuta.h@gmail.com>
2023-05-14 01:38:25 -0400

    se agrega programa 'working with json file'

---
+++ Encoding Programs/working-with-json-file.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+type Drink struct {
+	Name string `json:"name"`
+	Price int `json:"price"`
+}
+
+type Menu struct {
+	Drinks []Drink `json:"drinks"`
+}
+
+
+func main() {
+	// read the json file
+	fileBytes, err := ioutil.ReadFile("menu.json")
+	if err != nil {
+		fmt.Println("No se pudo encontrar el archivo especificado")
+		panic(err)
+	}
+
+	// decode the JSON file to a Menu object
+	var menu Menu
+	err = json.Unmarshal(fileBytes, &menu)
+	if err != nil {
+		panic(err)
+	}
+
+	// mostrar el nombre y costo del primer objeto del array menu
+	fmt.Printf("%s - Price $ %d", menu.Drinks[0].Name, menu.Drinks[0].Price)
+	
+}
