93eb84d8e6e956f06f6d32d38c1010807dd14bf9
pablozuta <pablo.zuta.h@gmail.com>
2023-06-12 17:17:39 -0400

    se agrega 'dice roll simulator'

--- Probability Programs/dice-roll-simulator.go
+++ Probability Programs/dice-roll-simulator.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"math/rand"
+	"time"
 )
 
 func main() {
@@ -13,6 +13,19 @@ func main() {
 	const numRolls = 1000
 	// iniciamos un array de 6 elementos a su zero-value
 	var frequencies [6]int
-	
-	
+
+	// generamos un numero random entre 0 y 5 y agregamos 1 para obtener un numero entre 0 y 6 (inclusive)
+	for i := 0; i < numRolls; i++ {
+		roll := rand.Intn(6) + 1
+		// incrementamos la cuenta en el elemento correspondiente del array
+		frequencies[roll-1]++
+	}
+
+	for i, frequency := range frequencies {
+		// calculamos la probabilidad al dividir el numero por el numero de rolls
+		probability := float64(frequency) / float64(numRolls)
+		// mostramos el numero, la frecuencia y la probabilidad
+		fmt.Printf("Numero %d: Frecuencia: %d, Probabilidad: %.2f\n", i+1, frequency, probability)
+	}
+
 }
