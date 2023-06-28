389e1e07b74f2469aac9389d5f1388735a88b57a
pablozuta <pablo.zuta.h@gmail.com>
2023-06-28 16:40:25 -0400

    se agrega carpeta de proyectos Constants Programs

---
+++ Constants Programs/area-of-circle.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	const pi = 3.14159
+	radius := 2.5
+
+	area := pi * (radius * radius)
+	fmt.Println("The area of a circle with radius 2.5 is:", area)
+}
