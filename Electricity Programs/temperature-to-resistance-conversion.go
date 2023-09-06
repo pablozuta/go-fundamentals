3bf9d6cf82972954d1814b04b88f4b8def69d774
pablozuta <pablo.zuta.h@gmail.com>
2023-09-05 22:21:23 -0400

    se agrega 'temperature to resistance conversion' a Electricity Programs

---
+++ Electricity Programs/temperature-to-resistance-conversion.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func temperatureToResistance(tempCelsius, referenceResistance, betaCoefficient float64) float64 {
+	invTempKelvin := 1.0 / (tempCelsius + 273.15)
+	return referenceResistance * math.Exp(betaCoefficient*(invTempKelvin-1))
+}
+
+func main() {
+	fmt.Println("Temperature to resistance conversion (Thermistor)")
+
+	var tempCelsius, referenceResistance, betaCoefficient float64
+	fmt.Print("Enter temperature in celsius: ")
+	fmt.Scanln(&tempCelsius)
+	fmt.Print("Enter reference resistance (Ω): ")
+	fmt.Scanln(&referenceResistance)
+	fmt.Print("Enter beta coefficient: ")
+	fmt.Scanln(&betaCoefficient)
+
+	resistance := temperatureToResistance(tempCelsius, referenceResistance, betaCoefficient)
+	fmt.Printf("Resistance at %.2fºC: %.2f Ω\n", tempCelsius, resistance)
+
+}
