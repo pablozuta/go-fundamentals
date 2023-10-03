1d44648f3d11d9dc087783e5d54b81fb6e6df236
pablozuta <pablo.zuta.h@gmail.com>
2023-10-02 23:41:34 -0400

    se agrega carpeta de proyectos 'Go CookBook'

---
+++ 2-Go CookBook/Chapter One/Interfaces/pipes.go
@@ -0,0 +1,27 @@
+package interfaces
+
+import (
+	"io"
+	"os"
+)
+
+// PipeExample helps give some more examples of using io interfaces
+func PipeExample() error {
+	// the pipe reader and pipe writer implement io.Reader and io.Writer
+	r, w := io.Pipe()
+
+	// this need to be run in a separate go routine
+	// as it will block waiting for the reader
+	// close at the end for cleanup
+	go func() {
+		// for now we'll write something basic,
+		// this could also be used to encode json
+		// base64 encode, etc.
+		w.Write([]byte("testn"))
+		w.Close()
+	}()
+	if _, err := io.Copy(os.Stdout, r); err != nil {
+		return err
+	}
+	return nil
+}
